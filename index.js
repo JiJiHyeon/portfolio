1504e712dd3f448ed50f9c19cbf31b14556c02e3
이지현 <40253245+JiJiHyeon@users.noreply.github.com>
2018-06-21 21:13:25 +0900

    fix
    포트폴리오 메인 수정
    잡자 수정

--- index.js
+++ index.js
@@ -8,11 +8,24 @@ $(function(){
 	$('.col2 > a, .col2 > span').first().hover(function(){
 		$('.imgG > li:nth-of-type(2)').css('display','block');
 		$('.imgG > li:nth-of-type(1)').css('display','none');
+		$('.imgG > li:nth-of-type(3)').css('display','none');
 	},function(){
-		$('.imgG > li:nth-of-type(2)').css('display','none');
 		$('.imgG > li:nth-of-type(1)').css('display','block');
+		$('.imgG > li:nth-of-type(2)').css('display','none');
+		$('.imgG > li:nth-of-type(3)').css('display','none');
+
 	});
 
+	$('.one > ul li:nth-of-type(3)').hover(function(){
+		$('.imgG > li:nth-of-type(3)').css('display','block');
+		$('.imgG > li:nth-of-type(1)').css('display','none');
+		$('.imgG > li:nth-of-type(2)').css('display','none');
+	},function(){
+		$('.imgG > li:nth-of-type(1)').css('display','block');
+		$('.imgG > li:nth-of-type(2)').css('display','none');
+		$('.imgG > li:nth-of-type(3)').css('display','none');
+
+	});
 
 	$('footer i').hover(function(){
 		$('footer a').css('backgound-color','#939393');
